75f96303f8e8c2c08dc5eed951838f0408b4b4e6
anshuman <anshuman28mishra@gmail.com>
2018-06-26 21:18:28 +0530

    my first project

---
+++ Average/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 7 };
+            Console.WriteLine(array.Average());
+            Console.ReadLine();
+        }
+    }
+}
